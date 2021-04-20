1c0e196fcf49386a21aef5d458692181a87311f7
brenoos <breno.oliver07@gmail.com>
2021-04-19 21:24:34 -0300

    First Class

---
+++ lib/home/widgets/score_card/score_card.dart
@@ -0,0 +1,57 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/widgets/chart/chart_widget.dart';
+import 'package:flutter/material.dart';
+
+class ScoreCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        height: 136,
+        decoration: BoxDecoration(
+            color: AppColors.white,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              BoxShadow(
+                color: AppColors.black.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 10,
+                offset: Offset(5, 3),
+              )
+            ]),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: ChartWidget(),
+            ),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Vamos Começar",
+                      style: AppTextStyles.heading,
+                    ),
+                    Text(
+                      "Complete os desafio e avance em conhecimento",
+                      style: AppTextStyles.body,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
